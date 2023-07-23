3393097e2a20bbc11ae51ad7cb2c215a46714462
Teri11 <augustineteri157@gmail.com>
2023-07-23 20:28:16 +0300

    bulk commit

--- frontend/JS/main.js
+++ frontend/JS/main.js
@@ -1,30 +1,34 @@
-// NAME & contact delivery section TO REFRESH
+
+// Name & contact delivery section TO REFRESH start...
 const nameInput = document.getElementById('namee');
 const nameWarning = document.getElementById('nameWarning');
 const contactInput = document.getElementById('contactt');
 const contactWarning = document.getElementById('contactWarning');
 
 function handleNameInput() {
-    const value = nameInput.value;
-    const lettersOnly = /^[A-Za-z]+$/.test(value);
-    if (!lettersOnly) {
-        nameWarning.classList.remove('d-none');
-        nameInput.value = '';
-    } else {
-        nameWarning.classList.add('d-none');
-    }
+  const value = nameInput.value;
+  const lettersOnly = /^[A-Za-z]+$/.test(value);
+  if (!lettersOnly) {
+    nameWarning.classList.remove('d-none');
+    nameInput.value = '';
+  } else {
+    nameWarning.classList.add('d-none');
+  }
 }
 
 function handleContactInput() {
-    const value = contactInput.value;
-    const numbersOnly = /^\d*$/.test(value);
-    if (!numbersOnly) {
-        contactWarning.classList.remove('d-none');
-        contactInput.value = '';
-    } else {
-        contactWarning.classList.add('d-none');
-    }
+  const value = contactInput.value;
+  const numbersOnly = /^\d*$/.test(value);
+  if (!numbersOnly) {
+    contactWarning.classList.remove('d-none');
+    contactInput.value = '';
+  } else {
+    contactWarning.classList.add('d-none');
+  }
 }
 
 nameInput.addEventListener('input', handleNameInput);
 contactInput.addEventListener('input', handleContactInput);
+
+// NAME & contact delivery section TO REFRESH end....
+
